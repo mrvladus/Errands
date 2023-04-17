7ece58b87e11a12b75da474aebec0bea7ad01ccf
mrvladus <mrvladus@yandex.ru>
2023-04-17 18:54:37 +0300

    Separate utils

--- src/main.c
+++ src/main.c
@@ -1,30 +1,8 @@
 #include "global.h"
+#include "utils.h"
 #include "widgets/entry.h"
 #include "widgets/headerbar.h"
 #include "widgets/todolist.h"
-
-// ---------- Compatability functions ---------- //
-
-// Convert to a new gsettings todos-v2 format from versions 44.1.x to 44.2.x
-void ConvertSettings()
-{
-    // Get old todos and transfer them to a new key "todos-v2"
-    g_auto(GStrv) old_todos = g_settings_get_strv(settings, "todos");
-    // Create new array of arrays
-    GVariantBuilder g_var_builder;
-    g_variant_builder_init(&g_var_builder, G_VARIANT_TYPE_ARRAY);
-    for (int i = 0; old_todos[i]; i++) {
-        g_autoptr(GStrvBuilder) sub_builder = g_strv_builder_new();
-        g_strv_builder_add(sub_builder, old_todos[i]);
-        g_auto(GStrv) array = g_strv_builder_end(sub_builder);
-        GVariant* new_todo = g_variant_new_strv((const char* const*)array, -1);
-        g_variant_builder_add_value(&g_var_builder, new_todo);
-    }
-    // Finish building new todos array
-    GVariant* new_todos = g_variant_builder_end(&g_var_builder);
-    // Save new todos to gsettings
-    g_settings_set_value(settings, "todos-v2", new_todos);
-}
 
 // ---------- Main window ---------- //
 
