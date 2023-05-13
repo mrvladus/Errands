84ff920d2ea8975d91f347527f715f78616dd048
mrvladus <mrvladus@yandex.ru>
2023-05-13 14:20:00 +0300

    Update

--- src/sub_task.py
+++ src/sub_task.py
@@ -17,16 +17,19 @@ class SubTask(Gtk.Box):
         # Escape text and find URL's'
         if not Markup.is_escaped(text):
             self.text = Markup.escape(self.text)
-        self.text = Markup.find_url(self.text)
-        self.sub_task_text.props.label = self.text
+        # self.text = Markup.find_url(self.text)
+        self.sub_task_text.props.label = Markup.find_url(self.text)
         # Check if text crosslined and toggle checkbox
         if Markup.is_crosslined(self.text):
             self.sub_task_completed_btn.props.active = True
 
     @Gtk.Template.Callback()
     def on_completed_btn_toggled(self, btn):
+        # TODO: use label text instead self.text
         if Markup.is_crosslined(self.text) and btn.props.active:
             self.parent.n_sub_tasks_completed += 1
+            return
+        if not Markup.is_crosslined(self.text) and not btn.props.active:
             return
         if btn.props.active:
             self.text = Markup.add_crossline(self.text)
@@ -46,23 +49,32 @@ class SubTask(Gtk.Box):
         UserData.set(new_data)
         self.parent.sub_tasks.remove(self)
 
-    # @Gtk.Template.Callback()
-    # def on_sub_task_edit(self, entry):
-    #     old_text = self.props.title
-    #     new_text = GLib.markup_escape_text(entry.get_buffer().props.text)
-    #     if (
-    #         new_text == old_text
-    #         or new_text == ""
-    #         or new_text in UserData.get()["todos"][self.parent.props.title]["sub"]
-    #     ):
-    #         return
-    #     # Change sub-todo
-    #     self.update_data(old_text, new_text)
-    #     # Set new title
-    #     self.props.title = new_text
-    #     # Mark as uncompleted
-    #     self.sub_task_completed_btn.props.active = False
-    #     # Clear entry
-    #     entry.get_buffer().props.text = ""
-    #     # Hide popup
-    #     self.sub_task_popover.popdown()
+    @Gtk.Template.Callback()
+    def on_sub_task_edit(self, entry):
+        old_text = self.text
+        new_text = entry.get_buffer().props.text
+        # Escape text and find URL's'
+        new_text = Markup.escape(new_text)
+        # Return if text the same or empty
+        if (
+            new_text == old_text
+            or new_text == ""
+            or new_text in UserData.get()["todos"][self.parent.text]["sub"]
+        ):
+            return
+        # Change sub-task text
+        new_data = UserData.get()
+        for idx, task in enumerate(new_data["todos"][self.parent.text]["sub"]):
+            if task == old_text:
+                new_data["todos"][self.parent.text]["sub"][idx] = new_text
+                UserData.set(new_data)
+                break
+        # Set new text
+        self.text = Markup.find_url(new_text)
+        self.sub_task_text.props.label = self.text
+        # Mark as uncompleted
+        self.sub_task_completed_btn.props.active = False
+        # Clear entry
+        entry.get_buffer().props.text = ""
+        # Hide popup
+        self.sub_task_popover.popdown()
