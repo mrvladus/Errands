bcb0d25d9c9f3afda34ce70c70c9c9617b5f3056
mrvladus <mrvladus@yandex.ru>
2023-08-02 14:37:57 +0300

    Update scripts

--- build-aux/regenerate-translations.sh
+++ build-aux/regenerate-translations.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-echo "---------- UPDATING TRANSLATIONS... ----------"
-meson setup _build
+flatpak run --filesystem=home org.gnome.Sdk//44 <<EOF
+echo -e "\n\033[32;1m---------- UPDATING TRANSLATIONS ----------\033[0m\n"
 cd _build
 meson compile list-update-po
-cd ..
-echo "---------- DONE! ----------"
+echo -e "\n\033[32;1m------------------ DONE! ------------------\033[0m\n"
+EOF
--- build-aux/run-flatpak.sh
+++ build-aux/run-flatpak.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-echo "---------- RUNNING LIST... ----------"
+echo -e "\n\033[32;1m---------- BUILDING ERRANDS ---------\033[0m\n"
 flatpak run org.flatpak.Builder --user --install --force-clean _build io.github.mrvladus.List.json
+echo -e "\n\033[32;1m---------- RUNNING ERRANDS ----------\033[0m\n"
 flatpak run io.github.mrvladus.List
-echo "---------- DONE! ----------"
+echo -e "\n\033[32;1m------------- DONE! --------------\033[0m\n"
