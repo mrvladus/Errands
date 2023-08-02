37997c258a6bd587aead20efd74e716231eab731
mrvladus <mrvladus@yandex.ru>
2023-08-02 18:06:15 +0300

    Fix translation script

--- build-aux/regenerate-translations.sh
+++ build-aux/regenerate-translations.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 flatpak run --filesystem=home org.gnome.Sdk//44 <<EOF
 echo -e "\n\033[32;1m---------- UPDATING TRANSLATIONS ----------\033[0m\n"
-pwd
+meson setup _build
 cd _build
-pwd
 meson compile list-update-po
 cd ..
 echo -e "\n\033[32;1m------------------ DONE! ------------------\033[0m\n"
