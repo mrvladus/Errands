3e8876d7e0202f8c06c82f186d3a00eb0372a3ab
mrvladus <mrvladus@yandex.ru>
2023-10-17 21:18:10 +0300

    Open calendar func

--- src/task_details.py
+++ src/task_details.py
@@ -1,7 +1,8 @@
 # Copyright 2023 Vlad Krupinskii <mrvladus@yandex.ru>
 # SPDX-License-Identifier: MIT
 
-from gi.repository import Adw, Gtk
+import os
+from gi.repository import Adw, Gtk, GLib, Gio
 from .utils import Log
 
 
@@ -11,12 +12,27 @@ class TaskDetails(Adw.PreferencesWindow):
 
     def __init__(self, task) -> None:
         super().__init__()
+        self.task = task
         self.set_transient_for(task.window)
         self.present()
 
-    def _convert_to_ics(self):
+    def _delete_old_ics(self):
         pass
+
+    def _convert_to_ics(self):
+        ics_text: str = "hello"
+        cache_dir: str = os.path.join(GLib.get_user_cache_dir(), "list")
+        if not os.path.exists(cache_dir):
+            os.mkdir(cache_dir)
+        with open(os.path.join(cache_dir, f"{self.task.task['id']}.ics"), "w") as f:
+            f.write(ics_text)
 
     @Gtk.Template.Callback()
     def on_calendar_open(self, _btn):
-        pass
+        self._convert_to_ics()
+        file: Gio.File = Gio.File.new_for_path(
+            os.path.join(
+                GLib.get_user_cache_dir(), "list", f"{self.task.task['id']}.ics"
+            )
+        )
+        Gtk.FileLauncher.new(file).launch()
