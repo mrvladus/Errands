80a9614927b3cf17b0fa23a337f53ee4a48bf1a1
mrvladus <mrvladus@yandex.ru>
2023-10-29 16:55:00 +0300

    Fix dnd

--- errands/utils/tasks.py
+++ errands/utils/tasks.py
@@ -9,30 +9,34 @@ from errands.utils.data import UserDataTask
 
 def new_task(
     text: str,
-    id: str = str(uuid.uuid4()),
+    id: str = "",
     parent: str = "",
     completed: bool = False,
     deleted: bool = False,
     color: str = "",
     synced_caldav: bool = False,
-    start_date: str = datetime.datetime.now().strftime("%Y%m%dT%H%M%S"),
-    end_date: str = format(
-        datetime.datetime.now() + datetime.timedelta(days=1), "%Y%m%dT%H%M%S"
-    ),
+    start_date: str = "",
+    end_date: str = "",
     notes: str = "",
     percent_complete: int = 0,
     priority: int = 0,
 ) -> UserDataTask:
     task = {
-        "id": id,
+        "id": id if id else str(uuid.uuid4()),
         "parent": parent,
         "text": text,
         "color": color,
         "completed": completed,
         "deleted": deleted,
         "synced_caldav": synced_caldav,
-        "start_date": start_date,
-        "end_date": end_date,
+        "start_date": start_date
+        if start_date
+        else datetime.datetime.now().strftime("%Y%m%dT%H%M%S"),
+        "end_date": end_date
+        if end_date
+        else format(
+            datetime.datetime.now() + datetime.timedelta(days=1), "%Y%m%dT%H%M%S"
+        ),
         "notes": notes,
         "percent_complete": percent_complete,
         "priority": priority,
