b34c5e6da545c88e98b851926bf209343adf2eb6
mrvladus <mrvladus@yandex.ru>
2024-03-22 20:31:58 +0300

    add titles

--- errands/widgets/components/titled_separator.py
+++ errands/widgets/components/titled_separator.py
@@ -2,16 +2,21 @@
 # SPDX-License-Identifier: MIT
 
 import os
-from gi.repository import Gtk  # type:ignore
+from gi.repository import Gtk, GObject  # type:ignore
 
 
 @Gtk.Template(filename=os.path.abspath(__file__).replace(".py", ".ui"))
 class TitledSeparator(Gtk.Box):
     __gtype_name__ = "TitledSeparator"
 
+    # PROPERTIES
+    title = GObject.Property(type=str, default="")
+
     label: Gtk.Label = Gtk.Template.Child()
 
-    def __init__(self, title: str, margins: tuple[int, int, int, int]):
+    def __init__(
+        self, title: str = "", margins: tuple[int, int, int, int] = (0, 0, 0, 0)
+    ):
         super().__init__()
         self.label.set_label(title)
         self.set_margin_start(margins[0])
