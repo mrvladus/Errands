59b607b3c551bf2dbac25a349ef340876969d2d4
mrvladus <mrvladus@yandex.ru>
2024-03-27 10:43:34 +0300

    remove old code

--- errands/widgets/component.py
+++ errands/widgets/component.py
@@ -1,67 +1,8 @@
 # Copyright 2023-2024 Vlad Krupinskii <mrvladus@yandex.ru>
 # SPDX-License-Identifier: MIT
 
-import datetime
 from typing import Callable
-from errands.lib.utils import get_children
-from gi.repository import Gtk, Adw, GObject, GLib, Gio  # type:ignore
-
-
-class Box(Gtk.Box):
-    """
-    Gtk.Box with multiple children provided in list.
-    """
-
-    def __init__(self, children: list[Gtk.Widget] = [], **kwargs):
-        super().__init__(**kwargs)
-        for child in children:
-            self.append(child)
-
-    def for_each(self, callback: Callable):
-        """Call function for each child as argument"""
-        for child in get_children(self):
-            callback(child)
-
-
-class Button(Gtk.Button):
-    """
-    Gtk.Button which takes arguments:
-    - label
-    - icon
-    - callback for "clicked" signal
-    - shortcut.
-    """
-
-    def __init__(
-        self,
-        label: str = None,
-        icon_name: str = None,
-        on_click: Callable = None,
-        shortcut: str = None,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.label: str = label
-        content = Adw.ButtonContent()
-        if icon_name:
-            content.set_icon_name(icon_name)
-        if label:
-            content.set_label(label)
-        if on_click:
-            self.connect("clicked", on_click)
-        if icon_name:
-            self.set_child(content)
-        else:
-            self.set_label(label)
-        if shortcut:
-            ctrl = Gtk.ShortcutController(scope=1)
-            ctrl.add_shortcut(
-                Gtk.Shortcut(
-                    trigger=Gtk.ShortcutTrigger.parse_string(shortcut),
-                    action=Gtk.ShortcutAction.parse_string("activate"),
-                )
-            )
-            self.add_controller(ctrl)
+from gi.repository import Adw, Gio  # type:ignore
 
 
 class ConfirmDialog(Adw.MessageDialog):
