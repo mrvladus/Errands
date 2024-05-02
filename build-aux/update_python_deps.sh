25daa1e384d069b70c1a8e1f4e70f2357a2504ab
mrvladus <mrvladus@yandex.ru>
2024-05-02 11:03:25 +0300

    upd

--- build-aux/update_python_deps.sh
+++ build-aux/update_python_deps.sh
@@ -2,6 +2,6 @@
 
 DEPS="caldav lxml pip"
 IGNORE_INSTALLED="lxml,pip"
-RUNTIME="org.gnome.Sdk//45"
+RUNTIME="org.gnome.Sdk//46"
 
 build-aux/flatpak-pip-generator.py --ignore-installed=$IGNORE_INSTALLED --output build-aux/python3-modules $DEPS
