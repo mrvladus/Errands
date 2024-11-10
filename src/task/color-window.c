3dcecf5eb4fdbb5fdf8180c940a54f8f5ac853f9
mrvladus <mrvladus@yandex.ru>
2024-11-10 15:10:27 +0300

    Update color-window.c

--- src/task/color-window.c
+++ src/task/color-window.c
@@ -4,6 +4,7 @@
 #include "task.h"
 
 #include <glib/gi18n.h>
+#include <stdio.h>
 
 static void on_errands_color_window_close_cb(ErrandsColorWindow *win);
 static void on_errands_color_window_color_select_cb(GtkCheckButton *btn, ErrandsColorWindow *win);
@@ -18,77 +19,29 @@ static void errands_color_window_init(ErrandsColorWindow *self) {
   g_object_set(self, "title", _("Accent Color"), NULL);
   g_signal_connect(self, "closed", G_CALLBACK(on_errands_color_window_close_cb), NULL);
 
-  // --- Buttons --- //
-
-  // None button
-  GtkWidget *none_btn = gtk_check_button_new();
-  g_object_set(none_btn, "name", "none", NULL);
-  gtk_widget_add_css_class(none_btn, "accent-color-btn");
-  gtk_widget_add_css_class(none_btn, "accent-color-btn-none");
-  g_signal_connect(none_btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb), self);
-
-  // Blue button
-  GtkWidget *blue_btn = gtk_check_button_new();
-  g_object_set(blue_btn, "name", "blue", "group", none_btn, NULL);
-  gtk_widget_add_css_class(blue_btn, "accent-color-btn");
-  gtk_widget_add_css_class(blue_btn, "accent-color-btn-blue");
-  g_signal_connect(blue_btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb), self);
-
-  // Green button
-  GtkWidget *green_btn = gtk_check_button_new();
-  g_object_set(green_btn, "name", "green", "group", none_btn, NULL);
-  gtk_widget_add_css_class(green_btn, "accent-color-btn");
-  gtk_widget_add_css_class(green_btn, "accent-color-btn-green");
-  g_signal_connect(green_btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb), self);
-
-  // Yellow button
-  GtkWidget *yellow_btn = gtk_check_button_new();
-  g_object_set(yellow_btn, "name", "yellow", "group", none_btn, NULL);
-  gtk_widget_add_css_class(yellow_btn, "accent-color-btn");
-  gtk_widget_add_css_class(yellow_btn, "accent-color-btn-yellow");
-  g_signal_connect(yellow_btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb),
-                   self);
-
-  // Orange button
-  GtkWidget *orange_btn = gtk_check_button_new();
-  g_object_set(orange_btn, "name", "orange", "group", none_btn, NULL);
-  gtk_widget_add_css_class(orange_btn, "accent-color-btn");
-  gtk_widget_add_css_class(orange_btn, "accent-color-btn-orange");
-  g_signal_connect(orange_btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb),
-                   self);
-
-  // Red button
-  GtkWidget *red_btn = gtk_check_button_new();
-  g_object_set(red_btn, "name", "red", "group", none_btn, NULL);
-  gtk_widget_add_css_class(red_btn, "accent-color-btn");
-  gtk_widget_add_css_class(red_btn, "accent-color-btn-red");
-  g_signal_connect(red_btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb), self);
-
-  // Putple button
-  GtkWidget *purple_btn = gtk_check_button_new();
-  g_object_set(purple_btn, "name", "purple", "group", none_btn, NULL);
-  gtk_widget_add_css_class(purple_btn, "accent-color-btn");
-  gtk_widget_add_css_class(purple_btn, "accent-color-btn-purple");
-  g_signal_connect(purple_btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb),
-                   self);
-
-  // Brown button
-  GtkWidget *brown_btn = gtk_check_button_new();
-  g_object_set(brown_btn, "name", "brown", "group", none_btn, NULL);
-  gtk_widget_add_css_class(brown_btn, "accent-color-btn");
-  gtk_widget_add_css_class(brown_btn, "accent-color-btn-brown");
-  g_signal_connect(brown_btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb), self);
-
   // Box
   self->color_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-  gtk_box_append(GTK_BOX(self->color_box), none_btn);
-  gtk_box_append(GTK_BOX(self->color_box), blue_btn);
-  gtk_box_append(GTK_BOX(self->color_box), green_btn);
-  gtk_box_append(GTK_BOX(self->color_box), yellow_btn);
-  gtk_box_append(GTK_BOX(self->color_box), orange_btn);
-  gtk_box_append(GTK_BOX(self->color_box), red_btn);
-  gtk_box_append(GTK_BOX(self->color_box), purple_btn);
-  gtk_box_append(GTK_BOX(self->color_box), brown_btn);
+
+  // --- Buttons --- //
+
+  const char *const colors[8] = {"none",   "blue", "green",  "yellow",
+                                 "orange", "red",  "purple", "brown"};
+
+  GtkWidget *group_btn;
+  for (int i = 0; i < 8; i++) {
+    GtkWidget *btn = gtk_check_button_new();
+    g_object_set(btn, "name", colors[i], NULL);
+    gtk_widget_add_css_class(btn, "accent-color-btn");
+    char class_name[24];
+    sprintf(class_name, "%s%s", "accent-color-btn-", colors[i]);
+    gtk_widget_add_css_class(btn, class_name);
+    g_signal_connect(btn, "toggled", G_CALLBACK(on_errands_color_window_color_select_cb), self);
+    if (!strcmp(colors[i], "none"))
+      group_btn = btn;
+    else
+      g_object_set(btn, "group", group_btn, NULL);
+    gtk_box_append(GTK_BOX(self->color_box), btn);
+  }
 
   // Toolbar view
   GtkWidget *tb = adw_toolbar_view_new();
