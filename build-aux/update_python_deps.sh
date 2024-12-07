315cbf939b2346a6a065dd94291aae4c670610ef
mrvladus <mrvladus@yandex.ru>
2024-12-07 12:14:29 +0300

    update

--- build-aux/update_python_deps.sh
+++ build-aux/update_python_deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-./req2flatpak.py --requirements-file requirements.txt --target-platforms 312-x86_64 312-aarch64 > manifest.json
+./req2flatpak.py --requirements-file requirements.txt --target-platforms '312-x86_64' '312-aarch64' > manifest.json
