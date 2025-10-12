d90c5d0538fdd9b23831d7f34c10a29015bcbb0f
mrvladus <mrvladus@yandex.ru>
2025-10-12 15:03:03 +0300

    Update to 49 runtime

--- build-aux/update_python_deps.sh
+++ build-aux/update_python_deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-./req2flatpak.py --requirements-file requirements.txt --target-platforms '312-x86_64' '312-aarch64' > manifest.json
+./req2flatpak.py --requirements-file requirements.txt --target-platforms '313-x86_64' '313-aarch64' > python3-caldav.json
