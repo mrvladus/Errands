dd321b2cf198de819e88b16139599fd3ba57f5b7
mrvladus <mrvladus@yandex.ru>
2026-01-23 18:21:43 +0300

    Create setup-distrobox.sh

---
+++ tools/scripts/setup-distrobox.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if ! command -v distrobox-create >/dev/null 2>&1
+then
+    echo "Distrobox is not installed. Look for it here: https://github.com/89luca89/distrobox"
+    exit 1
+fi
+
+distrobox-create \
+    --name errands-distrobox \
+    --image archlinux:latest \
+    --additional-packages "gcc make pkgconf git vim neovim zed curl libadwaita libportal libical libsecret gtksourceview-5 blueprint-compiler clang"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to create distrobox"
+    exit 1
+fi
+
+distrobox-enter errands-distrobox
